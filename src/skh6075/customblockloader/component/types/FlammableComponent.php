68997505e64e64d7019d3db15143e1b84e4834f6
SongKiHo <project2056@naver.com>
2022-05-17 22:44:34 +0900

    init commit: Added FlammableComponent

---
+++ src/skh6075/customblockloader/component/types/FlammableComponent.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace skh6075\customblockloader\component\types;
+
+use InvalidArgumentException;
+use pocketmine\nbt\tag\CompoundTag;
+use skh6075\customblockloader\component\BlockComponent;
+
+class FlammableComponent extends BlockComponent{
+
+	public function __construct(
+		private int $burn_odds = 0,
+		private int $flame_odds = 0
+	){
+		parent::__construct("minecraft:flammable");
+	}
+
+	public function isValid() : void{
+		if($this->burn_odds < 0 || $this->flame_odds < 0){
+			throw new InvalidArgumentException("The value of burn_odds and flame_odds must be greater than or equal to 0");
+		}
+	}
+
+	public function toComponent() : CompoundTag{
+		return CompoundTag::create()
+			->setTag($this->getName(), CompoundTag::create()
+				->setInt("burn_odds", $this->burn_odds)
+				->setInt("flame_odds", $this->flame_odds)
+			);
+	}
+}
