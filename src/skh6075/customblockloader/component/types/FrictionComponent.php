f42bb5d3266914dfc84d0fab546406cc904d35c4
SongKiHo <project2056@naver.com>
2022-05-17 22:40:15 +0900

    init commit: Added LootComponent and FrictionComponent

---
+++ src/skh6075/customblockloader/component/types/FrictionComponent.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace skh6075\customblockloader\component\types;
+
+use InvalidArgumentException;
+use pocketmine\nbt\tag\CompoundTag;
+use skh6075\customblockloader\component\BlockComponent;
+
+class FrictionComponent extends BlockComponent{
+
+	public function __construct(private float $value = 0.1){
+		parent::__construct("minecraft:friction");
+	}
+
+	public function isValid() : void{
+		if($this->value > 1.0 || $this->value < 0.1){
+			throw new InvalidArgumentException("The friction value must be greater than 0.1 and less than 1.0.");
+		}
+	}
+
+	public function toComponent() : CompoundTag{
+		return CompoundTag::create()->setFloat($this->getName(), $this->value);
+	}
+}
---
+++ src/skh6075/customblockloader/component/types/GeometryComponent.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace skh6075\customblockloader\component\types;
+
+use pocketmine\nbt\tag\CompoundTag;
+use skh6075\customblockloader\component\BlockComponent;
+
+class GeometryComponent extends BlockComponent{
+
+	public function __construct(){
+		parent::__construct("minecraft:geometry");
+	}
+
+	public function toComponent() : CompoundTag{
+		//Block geometry only supports cube...
+		return CompoundTag::create()->setString($this->getName(), "cube");
+	}
+}
